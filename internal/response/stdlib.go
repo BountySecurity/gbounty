2d59e82642627cd2895321ffd0ea925fcb991a0f
Joan López de la Franca Beltran <joanjan14@gmail.com>
2024-11-20 23:41:15 +0100

    Response: Manually decode gzipped responses

--- internal/response/stdlib.go
+++ internal/response/stdlib.go
@@ -2,8 +2,10 @@ package response
 
 import (
 	"bytes"
+	"compress/gzip"
 	"io"
 	"net/http"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -24,8 +26,32 @@ func FromStdlib(res *http.Response) (Response, error) {
 		res.Body = io.NopCloser(bytes.NewBuffer(body))
 	}
 
+	isContentEncodingGzip := func(k string, vv []string) bool {
+		return strings.EqualFold(k, "Content-Encoding") &&
+			slices.ContainsFunc(vv, func(v string) bool {
+				return strings.EqualFold(v, "gzip")
+			})
+	}
+
+	decodeGzipBody := func() ([]byte, error) {
+		gz, err := gzip.NewReader(bytes.NewReader(body))
+		if err != nil {
+			return nil, err
+		}
+		defer gz.Close()
+		return io.ReadAll(gz)
+	}
+
 	headers := make(map[string][]string)
 	for key, values := range res.Header {
+		if isContentEncodingGzip(key, values) {
+			var err error
+			body, err = decodeGzipBody()
+			if err != nil {
+				return Response{}, err
+			}
+			continue // We don't want to add the Content-Encoding header.
+		}
 		headers[key] = values
 	}
 
